bf619ac1456433a4e5d2ec3c89420c8ae816b21b
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-22 23:25:16 +0200

    Test Nav barre

--- AppB2F/App.js
+++ AppB2F/App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const tab = createBottomTabNavigator();
+
+function AccueilScreen(){
+  return(
+<View style={styles.container}>
+    <Text>Accueil</Text>
+  </View>
+  )
+}
+
+function ParametreScreen(){
+  return(
+<View style={styles.container}>
+    <Text>Parametre</Text>
+  </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name == "Accueil"){
+            iconName = "home"
+          } else if (route.name == "Parametre"){
+            iconName = "settings"
+          }
+
+          return <Ionicons name={iconName} size={30}/>
+        }
+      })}
+      >
+        <tab.Screen name='Accueil' component={AccueilScreen}/>
+        <tab.Screen name='Parametre' component={ParametreScreen}/>
+      </tab.Navigator>
+    </NavigationContainer>
   );
 }
 
