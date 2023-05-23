21b4a10aa34881f74c08929c670ff420e831f345
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-23 11:15:42 +0200

    Nav barre intégrée

--- AppB2F/screens/Accueil.js
+++ AppB2F/screens/Accueil.js
@@ -1,16 +1,74 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { View, Text, SafeAreaView, Button} from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Accueil = () => {
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    return (
-        <SafeAreaView>
-            <Text>This is Home</Text>
-            <Button onPress= {() => navigation.navigate("Second")} title="Va sur le deuxieme ecran" />
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text>This is Home</Text>
+        <Button
+          onPress={() => navigation.navigate("Second")}
+          title="Va sur le deuxième écran"
+        />
+      </View>
+      <View style={styles.navBar}>
+        <TouchableOpacity style={styles.iconContainer}>
+          <FontAwesome5 name="shopping-basket" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <MaterialCommunityIcons name="map-marker" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Ionicons name="home" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <FontAwesome name="credit-card-alt" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Ionicons name="person" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Accueil
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#f2f2f2",
+    borderWidth: 1,
+    borderColor: "black",
+    paddingVertical: 10,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
+
+export default Accueil;
