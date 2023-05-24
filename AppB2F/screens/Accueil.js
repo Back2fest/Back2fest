f7255b8e5a349a7489c061c6babc2755f1ab00fb
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-24 16:03:34 +0200

    Top container fini

--- AppB2F/screens/Accueil.js
+++ AppB2F/screens/Accueil.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Button,
+  StatusBar,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -31,28 +32,47 @@ const Accueil = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <View style={styles.notificationContainer}>
+        <TouchableOpacity style={styles.circle} />
+        <TouchableOpacity style={styles.notificationIcon}>
+          <Ionicons name="notifications" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
       <View style={styles.content}>
         <Text>This is Home</Text>
         <Button onPress={goToCreditCard} title="Va sur le deuxième écran" />
       </View>
       <View style={styles.navBar}>
         <TouchableOpacity style={styles.iconContainer}>
-          <FontAwesome5 name="shopping-basket" size={24} color="black"onPress={goToFood}/>
+          <FontAwesome5
+            name="shopping-basket"
+            size={24}
+            color="black"
+            onPress={goToFood}
+          />
         </TouchableOpacity>
         <TouchableOpacity style={styles.iconContainer}>
-          <MaterialCommunityIcons name="map-marker" size={24} color="black" onPress={goToMap}/>
+          <MaterialCommunityIcons
+            name="map-marker"
+            size={24}
+            color="black"
+            onPress={goToMap}
+          />
         </TouchableOpacity>
         <TouchableOpacity style={styles.iconContainer}>
           <Ionicons name="home" size={24} color="#EF8536" />
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.iconContainer}
-          onPress={goToCreditCard}
-        >
+        <TouchableOpacity style={styles.iconContainer} onPress={goToCreditCard}>
           <FontAwesome name="credit-card-alt" size={20} color="black" />
         </TouchableOpacity>
         <TouchableOpacity style={styles.iconContainer}>
-          <Ionicons name="person" size={24} color="black" onPress={goToProfil}/>
+          <Ionicons
+            name="person"
+            size={24}
+            color="black"
+            onPress={goToProfil}
+          />
         </TouchableOpacity>
       </View>
     </SafeAreaView>
@@ -62,8 +82,25 @@ const Accueil = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+  },
+  notificationContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
+    paddingHorizontal: 10,
+    marginTop: StatusBar.currentHeight + 30, // Ajout d'une marge en fonction de la hauteur de la barre d'état
+    marginLeft: 25,
+    marginRight: 25,
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "red",
+  },
+  notificationIcon: {
+    marginRight: 10,
   },
   content: {
     flex: 1,
