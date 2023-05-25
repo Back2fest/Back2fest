53dc3392db10403896f56c89c1b773f6c789a9df
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-25 10:18:22 +0200

    Search Barre ajouté

--- AppB2F/screens/Accueil.js
+++ AppB2F/screens/Accueil.js
@@ -39,6 +39,11 @@ const Accueil = () => {
           <Ionicons name="notifications" size={24} color="black" />
         </TouchableOpacity>
       </View>
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={24} color="black" style={styles.searchIcon} />
+        <Text style={styles.searchText}>Rechercher</Text>
+        <Ionicons name="filter" size={24} color="black" style={styles.filterIcon} />
+      </View>
       <View style={styles.content}>
         <Text>This is Home</Text>
         <Button onPress={goToCreditCard} title="Va sur le deuxième écran" />
@@ -89,7 +94,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     paddingHorizontal: 10,
-    marginTop: StatusBar.currentHeight + 30, // Ajout d'une marge en fonction de la hauteur de la barre d'état
+    marginTop: StatusBar.currentHeight + 30,
     marginLeft: 25,
     marginRight: 25,
   },
@@ -97,10 +102,32 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
-    backgroundColor: "red",
+    backgroundColor: "#EF8536",
   },
   notificationIcon: {
     marginRight: 10,
+  },
+  searchBar: {
+    marginTop: 40,
+    flexDirection: "row",
+    height: 65,
+    marginHorizontal: 25,
+    borderRadius: 20,
+    borderColor: 'black',
+    borderWidth: 1,
+    alignItems: "center",
+    paddingHorizontal: 15,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  searchText: {
+    flex: 1,
+    color:'#B3B3B3'
+  },
+  filterIcon: {
+    marginLeft: 10,
+    color: '#B3B3B3',
   },
   content: {
     flex: 1,
