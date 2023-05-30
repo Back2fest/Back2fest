3afbbff5931598dd700e6ed2f688a51bc645c45a
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-30 11:51:36 +0200

    Page alert

---
+++ AppB2F/screens/Alerte.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const Alerte = () => {
+    return (
+        <SafeAreaView>
+            <Text>This is a Alert Area</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Alerte
