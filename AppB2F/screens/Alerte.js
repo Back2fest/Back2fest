2e5ef687cfdc6fbebcb6773fc2266f7d16e33d30
Antoine-Bourdeau <antoine.bourdeau-couturier@epitech.digital>
2023-05-30 11:59:48 +0200

    Alerte page bouton add

--- AppB2F/screens/Alerte.js
+++ AppB2F/screens/Alerte.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const Alerte = () => {
-    return (
-        <SafeAreaView>
-            <Text>This is a Alert Area</Text>
-        </SafeAreaView>
-    )
-}
+  const navigation = useNavigation();
 
-export default Alerte
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.backButton} onPress={goBack}>
+          <Ionicons name="arrow-back" size={24} color="#EF8536" />
+        </TouchableOpacity>
+        <Text style={styles.alertText}>Alerte déclenchée !</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+  },
+  backButton: {
+    marginRight: 10,
+    borderWidth: 2,
+    borderColor: "#EF8536",
+    borderRadius: 20,
+    padding: 5,
+  },
+  alertText: {
+    fontSize: 18,
+    paddingLeft: 38,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+export default Alerte;
